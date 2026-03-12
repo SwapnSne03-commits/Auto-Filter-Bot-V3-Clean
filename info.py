e5df4bf453600ad8999c41133ac2eb9210f4ee6f
SwapnSne03-commits <somsneha03@gmail.com>
2026-03-12 14:58:03 +0530

    Update info.py
    Some Bad word remove

--- info.py
+++ info.py
@@ -146,9 +146,9 @@ IGNORE_WORDS = (list(os.environ.get("IGNORE_WORDS").split(",")) if os.environ.ge
 IGNORE_WORDS = [
     "movies", "movie", "episode", "episodes", "south indian", "south indian movie",
     "south movie", "south indian", "web-series", "web series", "webseries", "hindi me bhejo",
-    "ful", ",", "!", "kro", "jaldi", "audio", "language", "mkv", "mp4", "web", "series",
+    "ful", "!", "kro", "jaldi", "audio", "language", "mkv", "mp4", "web", "series",
     "hollywood", "all", "bollywood", "south", "hd", "karo", "upload", "bhejo",
-    "fullepisode", "bengali", "please", "plz", "hindi", "seasons", "request", "#request", "send", "link", "dabbbed", "dubbed", "season",
+    "fullepisode", "bengali", "please", "plz", "hindi", "seasons", "request", "#request", "send", "link", "dabbbed", "dubbed",
 ]
 #Image Host 
 IMGBB_API_KEY = os.environ["IMGBB_API_KEY"]
