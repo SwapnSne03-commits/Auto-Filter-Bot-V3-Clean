ce3852a3635656a72586e573a67d08887778d334
SwapnSne03-commits <somsneha03@gmail.com>
2026-03-19 11:57:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2229,7 +2229,7 @@ async def advantage_spoll_choker(bot, query):
 	
         key = f"{chat_id}-{user_msg.id}"
 
-        temp.GETALL[key] = all_files
+        temp.GETALL[key] = all_files[:70]
         temp.SMART_FILTERS[key] = {
             "languages": sorted(smart_languages),
             "seasons": sorted(smart_seasons),
