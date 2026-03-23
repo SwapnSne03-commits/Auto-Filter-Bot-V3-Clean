d9b7fa5fffa667796ea1440df39f474e2d6c6daa
SwapnSne03-commits <somsneha03@gmail.com>
2026-03-23 12:28:27 +0530

    Update Script.py
    Update caption style 👋

--- Script.py
+++ Script.py
@@ -275,7 +275,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <code>{}</code>"""
     
-    CAPTION = """<b>{file_caption}\n\nUploaded By: <a href="https://t.me/graduate_request">[@Graduate_Movies]</a></b>"""
+    CAPTION = """<b>{file_caption}\n\n{metadata}\n\nᴊᴏɪɴ ᴜs: <a href="https://t.me/graduate_movies">[@Graduate_Movies]</a></b>"""
 
     IMDB_TEMPLATE_TXT = """
 🏷 <b>ᴛɪᴛʟᴇ: <a href={url}>{title}</a></b>
